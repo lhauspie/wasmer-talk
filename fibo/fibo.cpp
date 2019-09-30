74c514758ec9b512432a34845946d6a6ea4e5227
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2019-09-30 21:20:41 +0200

    fibo in rust and cpp

---
+++ fibo/fibo.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdint>
+
+static uint64_t fib(uint64_t n) {
+  if (n <= 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+  printf("%lu \n", fib(46));
+}
+
